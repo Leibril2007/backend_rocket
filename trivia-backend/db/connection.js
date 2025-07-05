009645025b0f8aaace3ae7a6c737356183be2f04
Dania Luna <dania@localhost.localdomain>
2025-07-05 12:13:11 -0600

    backend hecho y conectado de triviagame

--- trivia-backend/db/connection.js
+++ trivia-backend/db/connection.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Cargar las variables de entorno del .env
 
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'luna1907',
-  database: 'trivia'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
   if (err) {
     console.error('❌ Error de conexión a la DB:', err);
   } else {
-    console.log('✅ Base de datos conectada');
+    console.log('✅ Conectado a la base de datos de Clever Cloud');
   }
 });
 
