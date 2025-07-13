e306ec811e7155314ce1c8745a2ee67f27089e7e
Leibril2007 <ariabi.sr@gmail.com>
2025-07-13 12:58:53 -0600

    mejora

--- ecoTrivia-back/routes/resultadoRoutes.js
+++ ecoTrivia-back/routes/resultadoRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/', guardarResultado);
 router.get('/', obtenerResultados);
-router.post('/ranking', calcularRanking); // ← ¡Esta es la nueva ruta!
+router.post('/ranking', calcularRanking);
 
 module.exports = router;
 
