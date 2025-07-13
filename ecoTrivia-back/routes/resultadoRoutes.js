27394262bc8e749918b74893fbe71163cb1c3f6f
Leibril2007 <ariabi.sr@gmail.com>
2025-07-13 12:07:24 -0600

    se habilita render

--- ecoTrivia-back/routes/resultadoRoutes.js
+++ ecoTrivia-back/routes/resultadoRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { guardarResultado, obtenerResultados } = require('../controllers/resultadoController');
+const { guardarResultado, obtenerResultados, calcularRanking } = require('../controllers/resultadoController');
 
 const router = express.Router();
 
 router.post('/', guardarResultado);
 router.get('/', obtenerResultados);
+router.post('/ranking', calcularRanking); // ← ¡Esta es la nueva ruta!
 
 module.exports = router;
 
